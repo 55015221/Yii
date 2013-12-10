812a904290a1b0c75395483568d6761b232666bf
Bily <55015221@qq.com>
2013-12-09 17:55:08 -0800

    s

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+if (isset($_POST['PHPSESSID'])) {
+    $_COOKIE['PHPSESSID'] = $_POST['PHPSESSID'];
+}
+// change the following paths if necessary
+$yii = dirname(__FILE__) . './../vendor/framework/yii.php';
+$config = dirname(__FILE__) . '/protected/config/main.php';
+
+// remove the following line when in production mode
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
+require_once($yii);
+Yii::createWebApplication($config)->run();
