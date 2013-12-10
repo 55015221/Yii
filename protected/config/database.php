5bf7da1335e0102020774a7b6ead47c00cd318a5
Bily <55015221@qq.com>
2013-12-10 04:18:21 -0800

    1

--- protected/config/database.php
+++ protected/config/database.php
@@ -2,8 +2,8 @@
 
 //db配置文件
 return array(
-    'connectionString' => 'sqlite:' . dirname(__FILE__) . '/../data/cms.db',
-    //'connectionString' => 'mysql:host=localhost;dbname=h.me', //数据库DNS
+    //'connectionString' => 'sqlite:' . dirname(__FILE__) . '/../data/cms.db',
+    'connectionString' => 'mysql:host=localhost;dbname=yii', //数据库DNS
     'username' => 'root', //数据库用户
     'password' => '', //数据库密码
     'schemaCachingDuration' => 3306, //端口
