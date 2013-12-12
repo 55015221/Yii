8670d7678a74d231ee235e28fcb741c2b8687483
Bily <55015221@qq.com>
2013-12-11 18:08:00 -0800

    upload

--- protected/components/Tool.php
+++ protected/components/Tool.php
@@ -128,6 +128,7 @@ class Tool {
     /**
      * 生成文件名
      * 用户文件上传
+     * return string 
      */
     public static function generateFilePath($type = 'images', $url = false) {
         $filepath = Yii::app()->params['upload_path'] . '/' . $type . '/' . date('Ymd', time());
@@ -136,6 +137,44 @@ class Tool {
             return $base . '/' . $filepath;
         }
         return null;
+    }
+    
+    /**
+     * 文件上传
+     * $foreign_id => 外键id
+     * $modul => 模块
+     * $folder => 目录
+     * return array 
+     */
+    public static function upload($foreign_id,$module,$folder = 'images'){
+        try {
+            $fileData = CUploadedFile::getInstanceByName('Filedata');
+            if (!$fileData || $fileData->getHasError()) {
+                echo 'Error: Documento Invalido';
+                Yii::app()->end();
+            }
+            $name = time() . '_' . mt_rand(10000, 99999) . '.' . pathinfo($fileData->name, PATHINFO_EXTENSION);
+            $picture_file->saveAs(Tool::generateFilePath() . '/' . $name);
+            //插入数据库
+            $filename = Tool::generateFilePath($folder, true) . '/' . $name;
+            $picture = new Picture();
+            $picture->pic_alt = pathinfo($fileData->name, PATHINFO_BASENAME);
+            $picture->pic_path = $filename;
+            $picture->pic_module = $module;
+            $picture->pic_foreign_id = $foreign_id;
+            $picture->pic_create_time = date('Y-m-d H:i:s');
+            if($picture->save()){
+                return array(
+                    'pic_id'=>$picture->attributes['pic_id'],
+                    'filename'=>$filename,
+                );
+            }
+            //echo "FILEID:".$picture->attributes['pic_id'];
+            //echo "FILEID:".Tool::generateFilePath('images', true) . '/' . $picture_name;
+        } catch (Exception $e) {
+            echo 'Error: ' . $e->getMessage();
+        }
+        Yii::app()->end();
     }
 
     /**
